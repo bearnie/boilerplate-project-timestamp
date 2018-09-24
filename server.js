4eb8c0c0d57bfaaf474acb43b91382af772c8a7a
Bearnie <bearniezeborg@gmail.com>
2018-09-24 13:21:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/api/hello", function (req, res) {
 
 
 app.get("/api/timestamp/:date_string", (req, res) => {
-  if ( new Date(req.params.date_string) ==! undefined ) {
+  if ( new Date(req.params.date_string) !== undefined ) {
     var date = new Date(req.params.date_string);
     res.json(
       {"unix": date.getTime() + ", utc :" + date.toUTCString()}
